e210fbafb7d5e389be7a761f596749b678e0fcbc
qxzhou1010 <qxzhou1010@qq.com>
2022-09-06 22:51:04 +0800

    Add new solution for leetcode35

--- Demo01-BinarySearch/leetcode35.rs
+++ Demo01-BinarySearch/leetcode35.rs
@@ -36,7 +36,7 @@ impl Solution {
         return end as i32;
     }
 
-    pub fn search_insert_2022_0905_02(nums: Vec<i32>, target: i32) -> i32 {
+    pub fn search_insert_2022_0906(nums: Vec<i32>, target: i32) -> i32 {
         // return Result<usize, usize>
         // if find the target, return Ok(usize)
         // if not find, return Err(usize), the usize is the index of the target inserted 
